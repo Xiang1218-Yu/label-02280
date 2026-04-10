163d1e32b091b3eacb8a3c6c0a5042fd92709031
Balala <1061366940@qq.com>
2026-04-10 09:51:51 +0800

    feat: 新增

--- frontend-user/src/views/HomeView.vue
+++ frontend-user/src/views/HomeView.vue
@@ -6,6 +6,7 @@
   <div class="home-page">
     <HeroSection @scroll-down="scrollToStats" />
     <StatsSection ref="statsSectionRef" />
+    <VisitStats />
     <FeaturesSection />
   </div>
 </template>
@@ -14,6 +15,7 @@
 import { ref } from 'vue'
 import HeroSection from '@/components/HeroSection.vue'
 import StatsSection from '@/components/StatsSection.vue'
+import VisitStats from '@/components/VisitStats.vue'
 import FeaturesSection from '@/components/FeaturesSection.vue'
 import createLogger from '@/utils/logger'
 
